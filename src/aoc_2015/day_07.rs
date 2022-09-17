dc45074d991bdbb72fc9703f704fde379a725771
Niklas Bönisch <niklas.boenisch@gmail.com>
2022-09-18 00:13:31 +0200

    Day7 part 2 solved

--- src/aoc_2015/day_07.rs
+++ src/aoc_2015/day_07.rs
@@ -134,5 +134,111 @@ fn part1(input: &String) {
 }
 
 fn part2(input: &String) {
+    let lines = input.split("\n").collect::<Vec<&str>>();
+    let mut state: HashMap<&str, u16> = HashMap::new();
+    state.insert("b", 3176);
 
+    while !state.contains_key("a") {
+        for line in lines.clone() {
+            let mut x = line.split("->");
+            let left = x.nth(0).unwrap().trim();
+            let right = x.nth(0).unwrap().trim();
+            if state.contains_key(right) {
+                continue;
+            } else {
+                if left.contains("AND") {
+                    let mut temp = left.split("AND");
+                    let ll = temp.nth(0).unwrap().trim();
+                    let lr = temp.nth(0).unwrap().trim();
+                    if (state.contains_key(ll) || is_numeric(ll)) &&  (state.contains_key(lr) || is_numeric(lr)) {
+                        let lv = match state.get(ll) {
+                            Some(i) => *i,
+                            None => ll.parse::<u16>().unwrap(),
+                        };
+                        let rv = match state.get(lr) {
+                            Some(i) => *i,
+                            None => lr.parse::<u16>().unwrap(),
+                        };
+                        state.insert(right, lv & rv);
+                    } else {
+                        continue;
+                    }
+                } else if left.contains("OR") {
+                    let mut temp = left.split("OR");
+                    let ll = temp.nth(0).unwrap().trim();
+                    let lr = temp.nth(0).unwrap().trim();
+                    if (state.contains_key(ll) || is_numeric(ll)) &&  (state.contains_key(lr) || is_numeric(lr)) {
+                        let lv = match state.get(ll) {
+                            Some(i) => *i,
+                            None => ll.parse::<u16>().unwrap(),
+                        };
+                        let rv = match state.get(lr) {
+                            Some(i) => *i,
+                            None => lr.parse::<u16>().unwrap(),
+                        };
+                        state.insert(right, lv | rv);
+                    } else {
+                        continue;
+                    }                    
+                } else if left.contains("LSHIFT") {
+                    let mut temp = left.split("LSHIFT");
+                    let ll = temp.nth(0).unwrap().trim();
+                    let lr = temp.nth(0).unwrap().trim();
+                    if (state.contains_key(ll) || is_numeric(ll)) &&  (state.contains_key(lr) || is_numeric(lr)) {
+                        let lv = match state.get(ll) {
+                            Some(i) => *i,
+                            None => ll.parse::<u16>().unwrap(),
+                        };
+                        let rv = match state.get(lr) {
+                            Some(i) => *i,
+                            None => lr.parse::<u16>().unwrap(),
+                        };
+                        state.insert(right, lv << rv);
+                    } else {
+                        continue;
+                    }                    
+                } else if left.contains("RSHIFT") {
+                    let mut temp = left.split("RSHIFT");
+                    let ll = temp.nth(0).unwrap().trim();
+                    let lr = temp.nth(0).unwrap().trim();
+                    if (state.contains_key(ll) || is_numeric(ll)) &&  (state.contains_key(lr) || is_numeric(lr)) {
+                        let lv = match state.get(ll) {
+                            Some(i) => *i,
+                            None => ll.parse::<u16>().unwrap(),
+                        };
+                        let rv = match state.get(lr) {
+                            Some(i) => *i,
+                            None => lr.parse::<u16>().unwrap(),
+                        };
+                        state.insert(right, lv >> rv);
+                    } else {
+                        continue;
+                    }                    
+                } else if left.contains("NOT") {
+                    let mut temp = left.split(" ");
+                    let ll = temp.nth(1).unwrap().trim();
+                    if state.contains_key(ll) || is_numeric(ll){
+                        let lv = match state.get(ll) {
+                            Some(i) => *i,
+                            None => ll.parse::<u16>().unwrap(),
+                        };
+                        state.insert(right, !lv);
+                    } else {
+                        continue;
+                    }                    
+                } else {
+                    if state.contains_key(left) || is_numeric(left){
+                        let lv = match state.get(left) {
+                            Some(i) => *i,
+                            None => left.parse::<u16>().unwrap(),
+                        };
+                        state.insert(right, lv);
+                    } else {
+                        continue;
+                    }                   
+                }
+            }
+        }
+    }
+    println!("a={}", state.get("a").unwrap());
 }
