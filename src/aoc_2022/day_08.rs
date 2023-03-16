8c18d9d9f8b17a0772dfdc027cc0c407c7cc0645
Niklas Bönisch <niklas.boenisch@gmail.com>
2023-03-16 16:09:33 +0100

    Done day8 of 2022

--- src/aoc_2022/day_08.rs
+++ src/aoc_2022/day_08.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs, vec};
 
 const DAY: usize = 8;
 
@@ -12,11 +12,147 @@ pub fn run() {
 }
 
 pub fn part1(input: &str) -> usize {
-    todo!();
+
+    let mut grid = vec![];
+
+    for line in input.split('\n') {
+        let mut horizontal = vec![];
+        for c in line.chars() {
+            horizontal.push(c.to_digit(10).unwrap());
+        }
+        grid.push(horizontal);
+    }
+
+    let mut total_visible: usize = 2*grid.len()+2*(grid[0].len()-2);
+
+    for i in 1..grid.len()-1 {
+        for j in 1..grid[0].len()-1 {
+            // println!("shape: {},{}, x:{} y:{}", grid.len(), grid[0].len(), j, i);
+            let height = grid[i][j];
+            let mut visible: bool;
+            // Check left
+            let mut one_bigger = false;
+            for x in 0..j {
+                if grid[i][x] >= height {
+                    one_bigger = true;
+                    break;
+                }
+            }
+            visible = !one_bigger;
+
+            // Check right
+            let mut one_bigger = false;
+            for x in j+1..grid[i].len() {
+                if grid[i][x] >= height {
+                    one_bigger = true;
+                    break;
+                }
+            }
+            visible = visible || !one_bigger;
+
+            // Check top
+            let mut one_bigger = false;
+            for y in 0..i {
+                if grid[y][j] >= height {
+                    one_bigger = true;
+                    break;
+                }
+            }
+            visible = visible || !one_bigger;
+
+            //Check bottom
+            let mut one_bigger = false;
+            for y in i+1..grid.len() {
+                if grid[y][j] >= height {
+                    one_bigger = true;
+                    break;
+                }
+            }
+            visible = visible || !one_bigger;
+
+            if visible {
+                total_visible += 1;
+            }
+        }
+    }
+
+    total_visible
 }
 
-pub fn part2(input: &str) -> usize {
-    todo!();
+pub fn part2(input: &str) -> u32 {
+
+    let mut grid = vec![];
+
+    for line in input.split('\n') {
+        let mut horizontal = vec![];
+        for c in line.chars() {
+            horizontal.push(c.to_digit(10).unwrap());
+        }
+        grid.push(horizontal);
+    }
+
+    let mut heighest_scenic_score: u32 = 0;
+
+    for i in 1..grid.len()-1 {
+        for j in 1..grid[0].len()-1 {
+            // println!("shape: {},{}, x:{} y:{}", grid.len(), grid[0].len(), j, i);
+            let height = grid[i][j];
+            let mut scenic_score: u32 = 1;
+
+            let mut distance = 0;
+            
+            // Check left
+            for x in (0..j).rev() {
+                distance += 1;
+                if grid[i][x] >= height {
+                    break;
+                }
+            }
+            scenic_score *= distance;
+            // println!("distance:{}", distance);
+
+            distance = 0;
+            // Check right
+            for x in j+1..grid[i].len() {
+                distance += 1;
+                if grid[i][x] >= height {
+                    break;
+                }
+            }
+            scenic_score *= distance;
+            // println!("distance:{}", distance);
+
+            distance = 0;
+            // Check top
+            for y in (0..i).rev() {
+                distance += 1;
+                if grid[y][j] >= height {
+                    break;
+                }
+            }
+            scenic_score *= distance;
+            // println!("distance:{}", distance);
+
+            distance = 0;
+            //Check bottom
+            for y in i+1..grid.len() {
+                distance += 1;
+                if grid[y][j] >= height {
+                    break;
+                }
+            }
+            scenic_score *= distance;
+            // println!("distance:{}", distance);
+
+            // println!("x:{}, y:{}, scenic:{}, height:{}", j, i, scenic_score, height);
+
+            if scenic_score > heighest_scenic_score {
+                heighest_scenic_score = scenic_score;
+            }
+        }
+    }
+
+    heighest_scenic_score
 }
 
 #[cfg(test)]
@@ -36,6 +172,6 @@ mod tests {
         let path = format!("input/2022/day_{:02}_test_01.txt", DAY);
         let input = fs::read_to_string(path).expect("Wo Datei?");
         let t = part2(&input);
-        assert_eq!(t, 24933642);
+        assert_eq!(t, 8);
     }
 }
