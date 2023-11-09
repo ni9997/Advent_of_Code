9f397a60f7ac346ad1409377e202a438fac558bd
Niklas Bönisch <niklas.boenisch@gmail.com>
2023-11-09 14:09:09 +0100

    Done 2022 day 11

--- src/aoc_2022/day_11.rs
+++ src/aoc_2022/day_11.rs
@@ -1,12 +1,11 @@
-use std::{
-    cell::{Ref, RefCell},
-    rc::Rc,
-};
+use std::{cell::RefCell, rc::Rc};
 
 use crate::utils::get_input;
 
 const YEAR: usize = 2022;
 const DAY: usize = 11;
+
+type DividerType = i64;
 
 #[allow(dead_code)]
 pub fn run() {
@@ -16,12 +15,15 @@ pub fn run() {
     println!("The result of part 2 is: {}", part2(&input));
 }
 
+#[derive(Debug)]
 struct MonkeyGang {
     monkeys: Vec<Rc<RefCell<Monkey>>>,
+    mod_enable: bool,
+    mod_val: usize,
 }
 
 impl MonkeyGang {
-    fn new(input: &str) -> MonkeyGang {
+    fn new(input: &str, divider: DividerType, mod_enable: bool) -> MonkeyGang {
         let mut monkeys = vec![];
         let monkey_strings = input.split("\n\n");
         for monkey_string in monkey_strings {
@@ -34,9 +36,9 @@ impl MonkeyGang {
                 .expect("Not properly formattet input")
                 .replace("  Starting items: ", "")
                 .split(", ")
-                .map(|x| Item::new(x.parse::<i32>().unwrap()))
+                .map(|x| Item::new(x.parse::<DividerType>().unwrap(), divider))
                 .collect::<Vec<Item>>();
-            println!("{:?}", items);
+            // println!("{:?}", items);
             let op_line = lines
                 .next()
                 .expect("Not properly formattet input")
@@ -54,7 +56,7 @@ impl MonkeyGang {
                     panic!("Wrong format")
                 }
             };
-            println!("{:?}", operation);
+            // println!("{:?}", operation);
             let test_divider = lines
                 .next()
                 .expect("Wrong format")
@@ -73,32 +75,43 @@ impl MonkeyGang {
                 .replace("    If false: throw to monkey ", "")
                 .parse::<usize>()
                 .unwrap();
-            println!(
-                "Div: {}, true target: {}, false target: {}",
-                test_divider, true_monkey_target, false_monkey_target
-            );
+            // println!(
+            //     "Div: {}, true target: {}, false target: {}",
+            //     test_divider, true_monkey_target, false_monkey_target
+            // );
             monkeys.push(Rc::new(RefCell::new(Monkey::new(
                 items,
                 test_divider,
                 operation,
+                true_monkey_target,
+                false_monkey_target,
             ))));
         }
-        let gang = MonkeyGang { monkeys };
-        gang
+        let mod_val = match mod_enable {
+            true => {
+                monkeys.iter().map(|x|x.borrow().test_divider).product()
+            },
+            false => {0},
+        };
+        // println!("Mod Val: {}", mod_val);
+        MonkeyGang { monkeys, mod_val, mod_enable }
     }
 
-    fn make_round(&mut self) {
+    fn make_round(&self) {
         for m in &self.monkeys {
             let mut monkey = m.as_ref().borrow_mut();
             monkey.turn();
         }
-        todo!()
+        // todo!()
     }
 
-    fn run(&mut self, rounds: usize) {
+    fn run(&self, rounds: usize) {
         for _ in 0..rounds {
             self.make_round();
         }
+        // for (i, m) in self.monkeys.iter().enumerate() {
+        //     println!("Monkey {} inspected {} times", i, m.borrow().inspection_count);
+        // }
     }
 
     fn get_monkey_business(&self, amount: usize) -> usize {
@@ -111,20 +124,11 @@ impl MonkeyGang {
         });
         let test2 = test
             .iter()
+            .rev()
             .take(amount)
             .map(|x| x.as_ref().borrow().inspection_count)
             .product();
         test2
-    }
-
-    fn print_inspections(&self) {
-        for (i, m) in self.monkeys.iter().enumerate() {
-            println!(
-                "Monkey {:02} inspected {:04} items",
-                i,
-                m.as_ref().borrow().inspection_count
-            );
-        }
     }
 
     fn update_monkeys(gang: Rc<RefCell<MonkeyGang>>) {
@@ -138,9 +142,9 @@ impl MonkeyGang {
 
 #[derive(Debug, Clone)]
 enum Operation {
-    MultiplyNumber(i32),
+    MultiplyNumber(DividerType),
     MultiplyOld,
-    Add(i32),
+    Add(DividerType),
 }
 
 enum DivisibleResult {
@@ -148,44 +152,69 @@ enum DivisibleResult {
     False,
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 struct Monkey {
     items: Vec<Item>,
     inspection_count: usize,
     test_divider: usize,
     operation: Operation,
     monkey_gang: Option<Rc<RefCell<MonkeyGang>>>,
+    true_monkey_target: usize,
+    false_monkey_target: usize,
 }
 
 impl Monkey {
-    fn new(items: Vec<Item>, test_divider: usize, operation: Operation) -> Monkey {
+    fn new(
+        items: Vec<Item>,
+        test_divider: usize,
+        operation: Operation,
+        true_monkey_target: usize,
+        false_monkey_target: usize,
+    ) -> Monkey {
         Monkey {
             items,
             inspection_count: 0,
             test_divider,
             operation,
             monkey_gang: None,
+            true_monkey_target,
+            false_monkey_target,
         }
     }
 
     fn turn(&mut self) {
-        while !self.items.is_empty() {
-            let mut current_item = self.items.pop().unwrap();
-            current_item.process_inspection(&self.operation);
+        while let Some(mut current_item) = self.items.pop() {
+            self.inspection_count += 1;
+            {
+                let gang = self.monkey_gang.clone().unwrap();
+                let gang = gang.borrow();
+                current_item.process_inspection(&self.operation, &gang.mod_enable, &gang.mod_val);
+            }
+            match self.test(&current_item) {
+                DivisibleResult::True => {
+                    let monkeys = self.monkey_gang.clone().unwrap().clone();
+                    let test = monkeys.borrow();
+                    let test2 = &test.monkeys;
+                    let test3 = test2[self.true_monkey_target].clone();
+                    Self::throw(current_item, test3);
+                },
+                DivisibleResult::False => {
+                    let monkeys = self.monkey_gang.clone().unwrap().clone();
+                    let test = monkeys.borrow();
+                    let test2 = &test.monkeys;
+                    let test3: Rc<RefCell<Monkey>> = test2[self.false_monkey_target].clone();
+                    Self::throw(current_item, test3);
+                },
+            }
         }
     }
 
-    fn throw(item: Item, monkey: Monkey) {
-        todo!()
+    fn throw(item: Item, monkey: Rc<RefCell<Monkey>>) {
+        monkey.borrow_mut().items.push(item);
     }
 
-    fn inspect(&self, item: &mut Item) {
-        item.process_inspection(&self.operation);
-        todo!();
-    }
-
-    fn test(&self, item: Item) -> DivisibleResult {
-        match item.worry_level % (self.test_divider as i32) == 0 {
+    fn test(&self, item: &Item) -> DivisibleResult {
+        match item.worry_level % (self.test_divider as DividerType) == 0 {
             true => DivisibleResult::True,
             false => DivisibleResult::False,
         }
@@ -194,15 +223,16 @@ impl Monkey {
 
 #[derive(Clone, Copy, Debug)]
 struct Item {
-    worry_level: i32,
+    worry_level: DividerType,
+    divider: DividerType
 }
 
 impl Item {
-    fn new(worry_level: i32) -> Item {
-        Item { worry_level }
+    fn new(worry_level: DividerType, divider: DividerType) -> Item {
+        Item { worry_level, divider }
     }
 
-    fn process_inspection(&mut self, operation: &Operation) {
+    fn process_inspection(&mut self, operation: &Operation, mod_enable: &bool, mod_val: &usize) {
         match operation {
             Operation::MultiplyNumber(num) => {
                 self.worry_level *= num;
@@ -214,20 +244,29 @@ impl Item {
                 self.worry_level += num;
             }
         }
+        self.worry_level /= self.divider;
+        if *mod_enable {
+            self.worry_level %= *mod_val as i64;
+        }
     }
 }
 
 pub fn part1(input: &str) -> usize {
-    let gang = Rc::new(RefCell::new(MonkeyGang::new(input)));
+    let gang = Rc::new(RefCell::new(MonkeyGang::new(input, 3, false)));
     MonkeyGang::update_monkeys(gang.clone());
-    let mut temp = gang.borrow_mut();
+    let temp = gang.borrow();
     temp.run(20);
-    let x = temp.get_monkey_business(2);
-    x
+    
+    temp.get_monkey_business(2)
 }
 
 pub fn part2(input: &str) -> usize {
-    todo!();
+    let gang = Rc::new(RefCell::new(MonkeyGang::new(input, 1, true)));
+    MonkeyGang::update_monkeys(gang.clone());
+    let temp = gang.borrow();
+    temp.run(10_000);
+    
+    temp.get_monkey_business(2)
 }
 
 #[cfg(test)]
@@ -248,6 +287,6 @@ mod tests {
         let path = format!("input/2022/day_{:02}_test_01.txt", DAY);
         let input = fs::read_to_string(path).expect("Wo Datei?");
         let t = part2(&input);
-        assert_eq!(t, 0);
+        assert_eq!(t, 2713310158);
     }
 }
