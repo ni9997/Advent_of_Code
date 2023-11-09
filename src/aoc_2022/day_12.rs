58f601fb29369d4f13cf031f8e2c8d6f65d671ae
Niklas Bönisch <niklas.boenisch@gmail.com>
2023-11-09 23:58:08 +0100

    Done 2022 day 12

--- src/aoc_2022/day_12.rs
+++ src/aoc_2022/day_12.rs
@@ -1,5 +1,5 @@
 use core::fmt;
-use std::{cell::RefCell, rc::Rc};
+use std::{cell::RefCell, collections::VecDeque, rc::Rc};
 
 use crate::utils::get_input;
 
@@ -14,18 +14,61 @@ struct Grid {
 
 impl Grid {
     fn get_neighbours(&self, x: usize, y: usize) -> Vec<Rc<RefCell<Node>>> {
-        use std::cmp::{min, max};
-        let mut neighbours = Vec::with_capacity(8);
-        for x_i in max(x.saturating_sub(1), 0)..min(x+1, self.x_max) {
-            for y_i in max(y.saturating_sub(1), 0)..min(y+1, self.y_max) {
-                if x_i == x && y_i == y {
-                    continue;
-                }
-                let node = self.row_vec[y_i][x_i].clone();
-                neighbours.push(node);
+        use std::cmp::{max, min};
+        let mut neighbours = Vec::with_capacity(4);
+        for x_i in max(x.saturating_sub(1), 0)..=min(x + 1, self.x_max) {
+            if x_i == x {
+                continue;
             }
+            let node = self.row_vec[y][x_i].clone();
+            neighbours.push(node);
+        }
+        for y_i in max(y.saturating_sub(1), 0)..=min(y + 1, self.y_max) {
+            if y_i == y {
+                continue;
+            }
+            let node = self.row_vec[y_i][x].clone();
+            neighbours.push(node);
         }
         neighbours
+    }
+
+    fn solve(&self, x_start: usize, y_start: usize) -> usize {
+        let mut to_solve = VecDeque::with_capacity(self.x_max * self.y_max);
+        let start_node = self.row_vec[y_start][x_start].clone();
+        to_solve.push_back(start_node);
+
+        while let Some(test_node) = to_solve.pop_front() {
+            let neig = self.get_neighbours(test_node.borrow().x, test_node.borrow().y);
+            let valid_neig = neig.iter().filter(|x| {
+                x.borrow().value - 1 <= test_node.borrow().value
+                    && x.borrow().distance.borrow().is_none()
+            });
+            for n in valid_neig {
+                if n.borrow().distance.borrow().is_none() {
+                    *n.borrow().distance.borrow_mut() =
+                        Some(test_node.borrow().distance.borrow().unwrap() + 1);
+                    if n.borrow().node_type == NodeType::End {
+                        return n.borrow().distance.borrow().unwrap();
+                    } else {
+                        to_solve.push_back(n.clone());
+                    }
+                }
+            }
+        }
+        usize::MAX
+    }
+
+    fn reset(&self) {
+        for row in &self.row_vec {
+            for node in row {
+                let n = node.borrow();
+                match n.node_type {
+                    NodeType::Start => {}
+                    _ => *n.distance.borrow_mut() = None,
+                }
+            }
+        }
     }
 }
 
@@ -41,7 +84,7 @@ impl fmt::Display for Grid {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum NodeType {
     Start,
     End,
@@ -51,22 +94,24 @@ enum NodeType {
 #[derive(Debug)]
 struct Node {
     node_type: NodeType,
-    added: bool,
-    next_possible: Vec<Node>,
     value: u8,
     x: usize,
-    y:usize,
+    y: usize,
+    distance: Rc<RefCell<Option<usize>>>,
 }
 
 impl Node {
-    fn new(node_type: NodeType, value: char, x:usize, y:usize) -> Node {
+    fn new(node_type: NodeType, value: char, x: usize, y: usize) -> Node {
+        let distance = match node_type {
+            NodeType::Start => Rc::new(RefCell::new(Some(0))),
+            _ => Rc::new(RefCell::new(None)),
+        };
         Node {
             node_type,
-            added: false,
-            next_possible: vec![],
             value: value as u8,
             x,
             y,
+            distance,
         }
     }
 }
@@ -98,41 +143,79 @@ pub fn part1(input: &str) -> usize {
     let y_count = input.chars().filter(|x| *x == '\n').count() + 1;
     let mut row_vec = Vec::with_capacity(y_count);
 
-    let mut start_x;
-    let mut start_y;
-
     let mut x_max = 0;
-    let y_max;
+    let mut start_node = None;
 
     for (y, line) in lines.enumerate() {
         let mut line_nodes = Vec::with_capacity(line.len());
         for (x, c) in line.chars().enumerate() {
-            line_nodes.push(Rc::new(RefCell::new(match c {
+            line_nodes.push(match c {
                 'S' => {
-                    start_x = x;
-                    start_y = y;
-                    Node::new(NodeType::Start, 'a', x, y)
+                    start_node = Some(Rc::new(RefCell::new(Node::new(NodeType::Start, 'a', x, y))));
+                    start_node.clone().unwrap()
                 }
-                'E' => Node::new(NodeType::End, 'z', x, y),
-                elevation => Node::new(NodeType::Normal, elevation, x, y),
-            })));
+                'E' => Rc::new(RefCell::new(Node::new(NodeType::End, 'z', x, y))),
+                elevation => Rc::new(RefCell::new(Node::new(NodeType::Normal, elevation, x, y))),
+            });
         }
         x_max = line_nodes.len();
         row_vec.push(line_nodes);
     }
-    y_max = row_vec.len();
+    let y_max = row_vec.len();
 
-    let grid = Grid { row_vec, x_max, y_max };
-
-    println!("{}", grid);
-
-    println!("{:?}", grid.get_neighbours(0, 0));
-
-    todo!();
+    let grid = Grid {
+        row_vec,
+        x_max: x_max - 1,
+        y_max: y_max - 1,
+    };
+    grid.solve(
+        start_node.clone().unwrap().borrow().x,
+        start_node.clone().unwrap().borrow().y,
+    )
 }
 
 pub fn part2(input: &str) -> usize {
-    todo!();
+    let lines = input.lines();
+    let y_count = input.chars().filter(|x| *x == '\n').count() + 1;
+    let mut row_vec = Vec::with_capacity(y_count);
+
+    let mut x_max = 0;
+    let mut start_nodes = vec![];
+
+    for (y, line) in lines.enumerate() {
+        let mut line_nodes = Vec::with_capacity(line.len());
+        for (x, c) in line.chars().enumerate() {
+            line_nodes.push(match c {
+                'S' | 'a' => {
+                    let start_node =
+                        Some(Rc::new(RefCell::new(Node::new(NodeType::Start, 'a', x, y))));
+                    start_nodes.push(start_node.clone().unwrap());
+                    start_node.clone().unwrap()
+                }
+                'E' => Rc::new(RefCell::new(Node::new(NodeType::End, 'z', x, y))),
+                elevation => Rc::new(RefCell::new(Node::new(NodeType::Normal, elevation, x, y))),
+            });
+        }
+        x_max = line_nodes.len();
+        row_vec.push(line_nodes);
+    }
+    let y_max = row_vec.len();
+
+    let grid = Grid {
+        row_vec,
+        x_max: x_max - 1,
+        y_max: y_max - 1,
+    };
+    start_nodes
+        .iter()
+        .map(|x| {
+            let test_node = x.borrow();
+            let result = grid.solve(test_node.x, test_node.y);
+            grid.reset();
+            result
+        })
+        .min()
+        .unwrap()
 }
 
 #[cfg(test)]
@@ -153,6 +236,6 @@ mod tests {
         let path = format!("input/{}/day_{:02}_test_01.txt", YEAR, DAY);
         let input = fs::read_to_string(path).expect("Wo Datei?");
         let t = part2(&input);
-        assert_eq!(t, 2713310158);
+        assert_eq!(t, 29);
     }
 }
