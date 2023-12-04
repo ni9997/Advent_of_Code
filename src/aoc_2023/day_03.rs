d98fdf47d4c54c3d71cb18c8edbf64ea3d6125c5
Niklas Bönisch <niklas.boenisch@gmail.com>
2023-12-04 16:04:43 +0100

    WIP

---
+++ src/aoc_2023/day_03.rs
@@ -0,0 +1,44 @@
+use crate::utils::get_input;
+
+const YEAR: usize = 2023;
+const DAY: usize = 3;
+
+
+
+#[allow(dead_code)]
+pub fn run() {
+    println!("Day {:02}", DAY);
+    let input = get_input(YEAR, DAY).unwrap();
+    println!("The result of part 1 is: {}", part1(&input));
+    println!("The result of part 2 is: {}", part2(&input));
+}
+
+pub fn part1(input: &str) -> usize {
+    todo!()
+}
+
+pub fn part2(input: &str) -> usize {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs;
+
+    #[test]
+    fn part1_test1() {
+        let path = format!("input/{}/day_{:02}_test_01.txt", YEAR, DAY);
+        let input = fs::read_to_string(path).expect("Wo Datei?");
+        let t = part1(&input);
+        assert_eq!(t, 8);
+    }
+
+    #[test]
+    fn part2_test1() {
+        let path = format!("input/{}/day_{:02}_test_01.txt", YEAR, DAY);
+        let input = fs::read_to_string(path).expect("Wo Datei?");
+        let t = part2(&input);
+        assert_eq!(t, 2286);
+    }
+}
