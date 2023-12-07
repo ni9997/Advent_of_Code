ac106a53553de6eb2d271d6afc30f312bb2dfd0c
Niklas Bönisch <niklas.boenisch@gmail.com>
2023-12-07 17:05:39 +0100

    2023 Day 3 part 1+2

--- src/aoc_2023/day_03.rs
+++ src/aoc_2023/day_03.rs
@@ -1,9 +1,45 @@
+use std::collections::HashSet;
+
 use crate::utils::get_input;
 
 const YEAR: usize = 2023;
 const DAY: usize = 3;
 
+#[derive(Debug)]
+struct PartNumber {
+    value: i64,
+    points: HashSet<(i64, i64)>,
+}
 
+impl PartNumber {
+    fn new(row: i64, col: i64, ch: char) -> Self {
+        let mut points = HashSet::new();
+        let value = (ch as u8 - b'0') as i64;
+        points.extend([
+            (row + 1, col - 1),
+            (row, col - 1),
+            (row - 1, col - 1),
+            (row + 1, col),
+            (row, col),
+            (row - 1, col),
+            (row + 1, col + 1),
+            (row, col + 1),
+            (row - 1, col + 1),
+        ]);
+        Self { value, points }
+    }
+
+    fn add_digit(&mut self, row: i64, col: i64, ch: char) {
+        self.value *= 10;
+        self.value += (ch as u8 - b'0') as i64;
+        self.points
+            .extend([(row, col + 1), (row - 1, col + 1), (row + 1, col + 1)]);
+    }
+
+    fn next_to_symbol(&self, syms: &HashSet<(i64, i64)>) -> bool {
+        self.points.intersection(syms).next().is_some()
+    }
+}
 
 #[allow(dead_code)]
 pub fn run() {
@@ -13,12 +49,83 @@ pub fn run() {
     println!("The result of part 2 is: {}", part2(&input));
 }
 
-pub fn part1(input: &str) -> usize {
-    todo!()
+pub fn part1(input: &str) -> i64 {
+    let mut nums: Vec<PartNumber> = vec![];
+    let mut syms: HashSet<(i64, i64)> = HashSet::new();
+
+    let mut cur_part: Option<PartNumber> = None;
+    for (row, line) in input.lines().enumerate() {
+        for (col, ch) in line.chars().enumerate() {
+            if ch.is_ascii_digit() {
+                if let Some(ref mut num) = cur_part {
+                    num.add_digit(row as i64, col as i64, ch);
+                } else {
+                    cur_part = Some(PartNumber::new(row as i64, col as i64, ch))
+                }
+            } else {
+                if let Some(num) = cur_part.take() {
+                    nums.push(num);
+                }
+                if ch != '.' {
+                    syms.insert((row as i64, col as i64));
+                }
+            }
+        }
+        if let Some(num) = cur_part.take() {
+            nums.push(num);
+        }
+    }
+
+    nums.iter()
+        .filter(|num| num.next_to_symbol(&syms))
+        .map(|x| x.value)
+        .sum::<i64>()
 }
 
-pub fn part2(input: &str) -> usize {
-    todo!()
+pub fn part2(input: &str) -> i64 {
+    let mut nums: Vec<PartNumber> = vec![];
+    let mut syms: HashSet<(i64, i64)> = HashSet::new();
+    let mut gears: HashSet<(i64, i64)> = HashSet::new();
+
+    let mut cur_part: Option<PartNumber> = None;
+    for (row, line) in input.lines().enumerate() {
+        for (col, ch) in line.chars().enumerate() {
+            if ch.is_ascii_digit() {
+                if let Some(ref mut num) = cur_part {
+                    num.add_digit(row as i64, col as i64, ch);
+                } else {
+                    cur_part = Some(PartNumber::new(row as i64, col as i64, ch))
+                }
+            } else {
+                if let Some(num) = cur_part.take() {
+                    nums.push(num);
+                }
+                if ch != '.' {
+                    syms.insert((row as i64, col as i64));
+                    if ch == '*' {
+                        gears.insert((row as i64, col as i64));
+                    }
+                }
+            }
+        }
+        if let Some(num) = cur_part.take() {
+            nums.push(num);
+        }
+    }
+
+    let mut total = 0;
+    for gear in &gears {
+        let mut matches = vec![];
+        for num in &nums {
+            if num.points.contains(gear) {
+                matches.push(num.value);
+            }
+        }
+        if matches.len() == 2 {
+            total += matches[0] * matches[1];
+        }
+    }
+    total
 }
 
 #[cfg(test)]
@@ -31,7 +138,7 @@ mod tests {
         let path = format!("input/{}/day_{:02}_test_01.txt", YEAR, DAY);
         let input = fs::read_to_string(path).expect("Wo Datei?");
         let t = part1(&input);
-        assert_eq!(t, 8);
+        assert_eq!(t, 4361);
     }
 
     #[test]
@@ -39,6 +146,6 @@ mod tests {
         let path = format!("input/{}/day_{:02}_test_01.txt", YEAR, DAY);
         let input = fs::read_to_string(path).expect("Wo Datei?");
         let t = part2(&input);
-        assert_eq!(t, 2286);
+        assert_eq!(t, 467835);
     }
 }
